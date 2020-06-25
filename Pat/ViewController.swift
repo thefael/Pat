2637d891af0c611e9d91fa2183ebfc8dc20b627b
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-06-25 01:10:29 -0300

    Pat project created

--- Pat/ViewController.swift
+++ Pat/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Returns the number of rows in section. Retrieve this value from
+        // the API to know how many races of dogs there are.
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Returns a configured cell using data retrieved from the API.
+        return UITableViewCell()
+    }
+    
+    
 }
 
