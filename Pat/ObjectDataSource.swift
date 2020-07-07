8114a9ec0f4d49cceb91ddd2d10643e66f42f7fe
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-07-07 15:24:54 -0300

    Fix ObjectDataSource class properties

--- Pat/ObjectDataSource.swift
+++ Pat/ObjectDataSource.swift
@@ -1,21 +1,19 @@
 import UIKit
 
 class ObjectDataSource<T, Cell: UITableViewCell>: NSObject, UITableViewDataSource {
-    var genericList: [T]?
-    var configureCell: (T, Cell) -> Void = {(item, cell) in }
+    var genericList = [T]()
+    var configureCell: ((T, Cell) -> Void)?
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let list = genericList else { return 0 }
-        return list.count
+        return genericList.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let list = genericList else { return UITableViewCell() }
-        let item = list[indexPath.row]
+        let item = genericList[indexPath.row]
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as? Cell else {
             return UITableViewCell()
         }
-        configureCell(item, cell)
+        configureCell?(item, cell)
         return cell
     }
 }
