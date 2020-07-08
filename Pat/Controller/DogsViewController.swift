124284f20c225ec420f53e76af7d7b0538c39aa1
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-07-08 01:04:37 -0300

    Add tableView to DogsViewController

--- Pat/Controller/DogsViewController.swift
+++ Pat/Controller/DogsViewController.swift
@@ -12,7 +12,5 @@ class DogsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 }
