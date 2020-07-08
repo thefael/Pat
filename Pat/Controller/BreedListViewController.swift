26e2a2b5878a2791759b17543af8f5bed10256fa
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-07-07 21:36:52 -0300

    Add dogs viewController and reorganize folders

--- Pat/Controller/BreedListViewController.swift
+++ Pat/Controller/BreedListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BreedListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
---
+++ Pat/Controller/DogsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DogsViewController.swift
+//  Pat
+//
+//  Created by Rafael Rodrigues on 07/07/20.
+//  Copyright © 2020 Rafael Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class DogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
