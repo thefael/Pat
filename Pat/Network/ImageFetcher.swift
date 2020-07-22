f33341ebd5733e298959e7f25bcee2d8abfb1f83
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-07-21 21:30:14 -0300

    Create image cache for dog images

--- Pat/Network/ImageFetcher.swift
+++ Pat/Network/ImageFetcher.swift
@@ -3,17 +3,24 @@ import UIKit
 class ImageFetcher {
     static let shared = ImageFetcher()
     private init() {}
+
+    var imageCache: [URL?: UIImage] = [:]
     
     func fetchImage(from imageURL: URL?, into imageView: UIImageView) {
-        if let dogImageURL = imageURL {
-            DispatchQueue.global(qos: .background).async {
-                do {
-                    let imageData = try Data(contentsOf: dogImageURL)
-                    DispatchQueue.main.async {
-                        imageView.image = UIImage(data: imageData)
+        if let cachedImage = imageCache[imageURL] {
+            imageView.image = cachedImage
+        } else {
+            if let dogImageURL = imageURL {
+                DispatchQueue.global(qos: .background).async {
+                    do {
+                        let imageData = try Data(contentsOf: dogImageURL)
+                        DispatchQueue.main.async {
+                            imageView.image = UIImage(data: imageData)
+                            self.imageCache[dogImageURL] = imageView.image
+                        }
+                    } catch {
+                        print("Unable to load data. \(error)")
                     }
-                } catch {
-                    print("Unable to load data. \(error)")
                 }
             }
         }
