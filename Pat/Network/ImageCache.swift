4752aa88ff325af4ca6e67388c922f6b2ccd6a84
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-07-23 18:36:21 -0300

    Isolate image cache

--- Pat/Network/ImageCache.swift
+++ Pat/Network/ImageCache.swift
@@ -5,21 +5,3 @@ class ImageCache {
 
     var imageCache: [URL?: UIImage] = [:]
 }
-
-extension UIImageView {
-    func fetchImage(from imageURL: URL?, _ completion: @escaping ((Result<UIImage, Error>) -> Void)) {
-        if let imageURL = imageURL {
-            let urlSession = URLSession.shared
-            let dataTask = urlSession.dataTask(with: imageURL) { data, _, error in
-                DispatchQueue.main.async {
-                    if let error = error {
-                        completion(.failure(error))
-                    } else if let data = data, let image = UIImage(data: data) {
-                        completion(.success(image))
-                    }
-                }
-            }
-            dataTask.resume()
-        }
-    }
-}
