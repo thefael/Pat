f30e9dd8b3826943d7b1cc8ad2d38fa269707eba
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-08-01 18:20:12 -0300

    Create Tab view controller and FavouritesListVC

---
+++ Pat/FavouritesList/FavouritesListViewController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class FavouritesListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
