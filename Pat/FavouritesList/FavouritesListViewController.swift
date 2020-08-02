8a30b78d1b89bf5c0514a1ecc511ee7a0bbb656d
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-08-01 22:05:37 -0300

    Create FavouriteCell

--- Pat/FavouritesList/FavouritesListViewController.swift
+++ Pat/FavouritesList/FavouritesListViewController.swift
@@ -1,10 +1,17 @@
-
 import UIKit
 
 class FavouritesListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    
+    let dataSource = ObjectDataSource<[UIImage], FavouriteCell>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+    }
+
+    func configureTableView() {
+        tableView.register(FavouriteCell.self, forCellReuseIdentifier: "ReusableCell")
+        tableView.dataSource = dataSource
+        tableView.rowHeight = 100
     }
 }
