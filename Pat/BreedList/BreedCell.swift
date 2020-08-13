20207deca4bef4abd4e3901b549542c3e780b01b
Rafael Rodrigues <rafael-rodrigues-@live.com>
2020-08-12 23:32:38 -0300

    Create custom breed cell with fave button

---
+++ Pat/BreedList/BreedCell.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class BreedCell: UITableViewCell {
+    let breedLabel = UILabel()
+    let favButton = UIButton()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        favButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        favButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        favButton.addTarget(self, action: #selector(handleMarkAsFavourite), for: .touchUpInside)
+        accessoryView = favButton
+    }
+
+    @objc private func handleMarkAsFavourite() {
+        toggleButton()
+    }
+
+    func toggleButton() {
+        favButton.isSelected = !favButton.isSelected
+        guard let onImg = UIImage(systemName: "heart.fill"),
+            let offImg = UIImage(systemName: "heart") else { return }
+        DispatchQueue.main.async {
+            self.setButtonImage(view: self.favButton,
+            onImg: onImg,
+            offImg: offImg,
+            onOffStatus: self.favButton.isSelected)
+        }
+    }
+
+    func setButtonImage(view: UIButton, onImg: UIImage, offImg: UIImage, onOffStatus: Bool ) {
+        switch onOffStatus {
+        case true:
+            view.setImage(onImg, for: .normal)
+            print("Button Pressed")
+        default:
+            view.setImage(offImg, for: .normal)
+            print("Button Unpressed")
+        }
+        self.setNeedsLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+
+    }
+}
---
+++ Pat/FavouritesList/FavouritesList.swift
@@ -0,0 +1,9 @@
+//
+//  FavouritesList.swift
+//  Pat
+//
+//  Created by Rafael Rodrigues on 11/08/20.
+//  Copyright © 2020 Rafael Rodrigues. All rights reserved.
+//
+
+import Foundation
